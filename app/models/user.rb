ac65723df8f7484fc5a90915b98492d2c1cac67b
Wagner Wieland <wagnerp5@hotmail.com>
2023-03-03 10:46:18 -0300

    :)

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,11 +3,6 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-         has_many :offers
-         has_many :bookings
-         has_many :bookings, through: :offers, source: :bookings
-         validates :username, :password, :email, presence: true
-         validates :username, :password, length: { minimum: 6}
-         validates :username, :email, uniqueness: true
-         validates :email, format: { with: /\A.*@.*\.com\z/ }
+         has_many  :offers
+         has_many  :bookings
 end
